29471ac049ae1f607fa63e656e6d40aa15b8c82c
AlexDv <DvoinikovAV@yandex.ru>
2024-01-24 10:25:38 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {task1();
+
+    }
+    // Объявить метод «сгенерироватьМассив»
+    public static int[] generateRandomArray()
+    {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1()
+    {
+        int [] arr=generateRandomArray();
+        int summ=0;
+        for(int elmArr:arr){
+            summ+=elmArr;
+
+        }
+        System.out.println("Сумма трат за месяц составила "+summ+" рублей");
+
     }
 }
